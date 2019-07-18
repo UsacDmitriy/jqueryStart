371c7191c693497d94979f84da64564c52cc6465
Your NameUsacDmitry <usachev2012@gmail.com>
2019-07-18 22:30:11 +0400

    first commit

--- 1 lesson/js/script.js
+++ 1 lesson/js/script.js
@@ -1,11 +1,21 @@
 window.onload = function(e) {
 
    let buttons =  document.querySelectorAll('.actions input');
-   console.log(buttons);
 
+
+   for (let index = 0; index < buttons.length; index++) {
+       buttons[index].onclick = btnClick;
+   }
 }
 
 
-function some(){
-    console.log(1);
+function btnClick(e){
+
+    /*for (let k in this){
+        document.body.innerHTML += '<b>' + k + '</b>' + '    ' + this[k] +'<br>';
+    }
+    */
+
+    let name = this.getAttribute('data-value');
+    console.log(name);
 }

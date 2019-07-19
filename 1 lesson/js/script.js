4d61fb5fd6826c16610d1784e97b03634b51c93c
Dmitry <usachev2012@gmail.com>
2019-07-19 17:14:24 +0400

    second

--- 1 lesson/js/script.js
+++ 1 lesson/js/script.js
@@ -1,11 +1,30 @@
 window.onload = function(e) {
 
-   let buttons =  document.querySelectorAll('.actions input');
+   /*let buttons =  document.querySelectorAll('.actions input');
+
 
 
    for (let index = 0; index < buttons.length; index++) {
        buttons[index].onclick = btnClick;
+   }*/
+
+   let a = ['a', 's', 'd', 'f'];
+
+   for (let index = 0; index < a.length; index++) {
+     console.log(a[index]);       
    }
+
+   let b = { 
+       russia: 'Moskow',
+       usa: 'Washington',
+       china: 'Beijing'
+   };
+
+   for (const i in b) {
+        console.log(b[i]);
+   }
+
+
 }
 
 
@@ -14,8 +33,16 @@ function btnClick(e){
     /*for (let k in this){
         document.body.innerHTML += '<b>' + k + '</b>' + '    ' + this[k] +'<br>';
     }
-    */
+    
 
     let name = this.getAttribute('data-value');
-    console.log(name);
+    
+    if (name == 'save'){
+        console.log(1);
+    } else if (name == 'delete'){
+        console.log('remove object');
+    }*/
+
+
+
 }

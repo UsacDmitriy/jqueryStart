e115aafa6ee80b9cb896421d40f4cfbba77750f6
Dmitry <usachev2012@gmail.com>
2019-07-26 18:19:59 +0400

    introduction

--- Introduction/scripts/script.js
+++ Introduction/scripts/script.js
@@ -9,9 +9,25 @@ function printObject(object){
     document.write(res);
 }
 
-printObject(text.style);
+let images = document.querySelectorAll('#gallery .photos img');
+let btnPrev = document.querySelector("#gallery .buttons .prev");
+let btnNext = document.querySelector("#gallery .buttons .next");
+let i = 0;
 
-text.onclick = function(){
-    text.innerHTML = 'red';
-    text.className = 'test';
-}
+btnNext.addEventListener('click', ()=>{
+    images[i].classList.remove('showed');
+    i++;
+    if (i > (images.length - 1)){
+        i = 0;
+    }
+    images[i].classList.add('showed');
+});
+
+btnPrev.addEventListener('click', ()=>{
+    images[i].classList.remove('showed');
+    i--;
+    if (i < 0){
+        i = images.length - 1;
+    }
+    images[i].classList.add('showed');
+});

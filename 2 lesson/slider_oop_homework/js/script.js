b1df29a352c218bb98617d96bde2bd4e07ebe683
Your NameUsacDmitry <usachev2012@gmail.com>
2019-07-29 06:45:52 +0400

    slider

--- 2 lesson/slider_oop_homework/js/script.js
+++ 2 lesson/slider_oop_homework/js/script.js
@@ -6,68 +6,51 @@ window.onload = function(e) {
         btnNext: '.gallery-1 .buttons .next',
         auto: false
     });
-
-    slider.slideShow();
-
-
+    let slider2 = new Slider({
+        images: '.gallery-2 .photos img',
+        btnPrev: '.gallery-2 .buttons .prev',
+        btnNext: '.gallery-2 .buttons .next',
+        auto: true
+    });
 }
 
 function Slider(obj){
-    this.obj = obj;
+    this.images = document.querySelectorAll(obj.images);
+    this.btnNext = obj.btnNext;
+    this.btnPrev = obj.btnPrev;
+    this.auto = obj.auto;
+    this.rate = obj.rate || 1000;
+    
     let i = 0;
     let self = this;
-    
-    this.start = function(){
-            self.prev();
-            self.next();       
-    }
      
     this.prev = function(){
-        let btn = document.querySelector(this.obj.btnPrev);
-        let i = 0;
-        btn.onclick = function(){
-            
-            console.log(self.obj.images);
-            let imgs = document.querySelectorAll(self.obj.images); 
-            console.log(imgs);
-            imgs[i].classList.remove('showed');
+
+            self.images[i].classList.remove('showed');
             i--;
-
+            console.log(this);
             if (i<0){
-                i = imgs.length - 1;
+                i = self.images.length - 1;
             }
-
-            imgs[i].classList.add('showed');
-            console.log(i);
-        }
+            self.images[i].classList.add('showed');        
     }
 
     this.next = function(){
-        let btn = document.querySelector(this.obj.btnNext);
-        let i = 0;
-        btn.onclick = function(){
-            console.log(self.obj.images);
-            let imgs = document.querySelectorAll(self.obj.images);
-            console.log(imgs);
-            imgs[i].classList.remove('showed');
+
+            self.images[i].classList.remove('showed');
             i++; 
-            if (i>(imgs.length-1)){
+            console.log(this);
+            if (i>(self.images.length-1)){
                 i = 0;
             }
-            imgs[i].classList.add('showed');
-            console.log(i);
-        }  
+            self.images[i].classList.add('showed');
+            console.log(i);    
     }
 
-    this.slideShow = function(){
-        setInterval(function(){
-            let imgs = document.querySelectorAll(self.obj.images);
-            imgs[i].classList.remove('showed');
-            i++; 
-            if (i>(imgs.length-1)){
-                i = 0;
-            }
-            imgs[i].classList.add('showed');
-        }, 2000);
+    document.querySelector(self.btnNext).onclick = self.next;
+    document.querySelector(self.btnPrev).onclick = self.prev;
+
+    if (self.auto){
+        setInterval(self.next, self.rate);
     }
 }

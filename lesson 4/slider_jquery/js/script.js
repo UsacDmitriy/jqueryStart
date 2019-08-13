98adbd8fc4615b1b4fc1430770fcb7b076742cd6
Your NameUsacDmitry <usachev2012@gmail.com>
2019-08-13 21:04:07 +0400

    homework_jquery

--- lesson 4/slider_jquery/js/script.js
+++ lesson 4/slider_jquery/js/script.js
@@ -1,18 +1,40 @@
 $(function () { 
 
     let i = 0;
+    let k = 0;
 
     $('.next').on('click', function () {
-        
+
+        $(`.gallery-1 .photos img:eq(${i})`).removeClass('showed');
+        i++;
         if ( i >= $(`.gallery-1 .photos img`).length) {
             i = 0;
         }
+        $(`.gallery-1 .photos img:eq(${i})`).addClass('showed');
 
-        console.log(i);
+    });
+    
+    $('.prev').on('click', function () {
+        
         $(`.gallery-1 .photos img:eq(${i})`).removeClass('showed');
-        i++;
+        i--;
+        if ( i < 0) {
+            i = $(`.gallery-1 .photos img`).length - 1;
+        }
         $(`.gallery-1 .photos img:eq(${i})`).addClass('showed');
 
     });
 
- })
+    setInterval(function () { 
+
+        $(`.gallery-2 .photos img:eq(${k})`).removeClass('showed');
+        k++;
+        if ( k >= $(`.gallery-2 .photos img`).length) {
+            k = 0;
+        }
+        $(`.gallery-2 .photos img:eq(${k})`).addClass('showed');
+
+     }, 1000);
+
+
+});
